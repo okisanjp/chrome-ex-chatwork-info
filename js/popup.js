8d742874e2f690b7a8438081d42ff3e70385697a
okisanjp <okisan.jp@gmail.com>
2017-05-14 14:12:59 +0900

    開いているタブを選択して文字列を生成できるようにした

--- js/popup.js
+++ js/popup.js
@@ -1,11 +1,31 @@
 $(function(){
-  chrome.tabs.query( {active: true, lastFocusedWindow: true}, function (tabs) {
-    var tab = tabs[0];
-    var link = '[info][title]' + tab.title + '[/title]' + tab.url + '[/info]\n';
-    $('#link').val(link);
-    var clipboard = new Clipboard('.btn');
+  chrome.tabs.query({currentWindow: true, pinned: false}, function (tabs) {
+    var i = 0;
+
+    tabs.forEach(function(tab) {
+
+      $('#linkList').append(
+        '<div class="col-sm-12 linkContentItem"><input type="checkbox" id="' +
+        i + '">&nbsp;&nbsp;<label for="' + i + '">' + tab.title + '</label></div>'
+      );
+      i++;
+    });
+
+    $('input').on('change', function(){
+      var id = $(this).attr("id");
+      var link = '[info][title]' + tabs[id].title + '[/title]' + tabs[id].url + '[/info]\n\n';
+      var currentContent = $('#postContent').val();
+      if ($(this).prop('checked')) {
+        $('#postContent').val(currentContent + link);
+      } else {
+        currentContent = currentContent.replace(link, '');
+        $('#postContent').val(currentContent);
+      }
+    });
   });
+
+  var clipboard = new Clipboard('.btn');
   clipboard.on('success', function(e) {
-    e.clearSelection();
+    window.close();
   });
 });
